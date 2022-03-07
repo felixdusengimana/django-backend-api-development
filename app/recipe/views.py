4ca21a9afdd564a2b6c536b1c99cb26225719386
phelixdusengimana <phelixdusengimana@gmail.com>
2022-03-07 11:33:39 +0200

    (feat): made a migration.

---
+++ app/recipe/views.py
@@ -0,0 +1,41 @@
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Tag, Ingredient
+from .serializers import TagSerializer, IngredientSerializer
+
+
+class TagViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
+    """Manage tags in database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        """Return object for parent authenticated objects."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create new tag."""
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin
+):
+    """Manage ingredients in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, )
+    queryset = Ingredient.objects.all()
+    serializer_class = (IngredientSerializer, )
+
+    def get_queryset(self):
+        """Return object for current authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
