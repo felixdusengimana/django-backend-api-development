f9a4c6dde74c5c24c93142ed7e0aac710fdfbe20
phelixdusengimana <phelixdusengimana@gmail.com>
2022-03-08 15:53:57 +0200

    (feat): detail view implemented.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,8 +2,8 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
-from .serializers import TagSerializer, IngredientSerializer
+from core.models import Tag, Ingredient, Recipe
+from .serializers import TagSerializer, IngredientSerializer, RecipeSerializer, RecipeDetailSerializer
 
 
 class BaseRecipeAttrViewSet(
@@ -36,3 +36,20 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
 
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipe in database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class."""
+        if self.action == 'retrieve':
+            return RecipeDetailSerializer
+        return self.serializer_class
